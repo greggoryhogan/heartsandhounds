34ea758f62aa54780e830a9331dbaa8f4e08feb7
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2025-05-03 21:08:49 -0400

    Woo theme styling

--- wp-content/themes/hh/functions.php
+++ wp-content/themes/hh/functions.php
@@ -185,4 +185,9 @@ if(!function_exists('is_staging')) {
 	}
 }
 
- 
+add_filter('loop_shop_columns', 'loop_columns');
+if (!function_exists('loop_columns')) {
+	function loop_columns() {
+		return 3; // 3 products per row
+	}
+}
--- wp-content/themes/hh/index.php
+++ wp-content/themes/hh/index.php
@@ -4,7 +4,9 @@ if ( have_posts() ) {
 	while ( have_posts() ) {
     	the_post(); 
 		echo '<div class="container">';
-			echo '<h1 class="pt-4 mb-3 m-0">'.get_the_title().'</h1>';
+			echo '<div class="text-center">';
+				echo '<h1 class="pt-4 pb-2 mb-4 m-0 border-bottom">'.get_the_title().'</h1>';
+			echo '</div>';
 			the_content();
 		echo '</div>';
 	} // end while
