903c0548c1e6563c8db7ce33ed809a366eafcc2f
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2025-05-03 21:40:43 -0400

    Remove related prods

--- wp-content/themes/hh/functions.php
+++ wp-content/themes/hh/functions.php
@@ -194,3 +194,4 @@ if (!function_exists('loop_columns')) {
 }
 
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
