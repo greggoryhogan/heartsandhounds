ffb6fc08e17df84b71edb7071348f2882030adce
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2025-07-14 15:45:08 -0400

    Container width

--- wp-content/themes/hh/index.php
+++ wp-content/themes/hh/index.php
@@ -3,7 +3,7 @@
 if ( have_posts() ) {
 	while ( have_posts() ) {
     	the_post(); 
-		echo '<div class="container-lg">';
+		echo '<div class="container-md">';
 			echo '<div class="text-center">';
 				echo '<h1 class="pt-4 pb-2 mb-4 m-0 border-bottom">'.get_the_title().'</h1>';
 			echo '</div>';
